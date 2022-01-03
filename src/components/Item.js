078c663c6063ddc4c6e9cca00189e2e59946548f
Kyle Goodwin <kyle.goodwin93@gmail.com>
2022-01-02 21:01:54 -0500

    commit tree 6394af04aedb066ca040c0c8739bb806e5de954e

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
+
+
 
 function Item({ name, category }) {
+
+  const [className, setClassName] = useState(false)
+
+  function handleClick(e){
+    setClassName(!className)
+  }
+
   return (
-    <li className="">
+    <li className={className ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={(e) => handleClick(e)}className="add">{className ? "Remove From Cart" : "Add to Cart"}</button>
     </li>
   );
 }
